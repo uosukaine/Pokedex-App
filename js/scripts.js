1cbd381287134c568773ae38f7e51b610dc3ab5d
uosukaine <uosukaine@gmail.com>
2022-03-17 14:59:30 +0100

    Changed index and scripts
    Made initial changes in the files according to assignment. Created README and added favicon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-let myName = "Bob";
-document.write(myName);
-myName = "John Doe";
-document.write(myName);
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-let size = 100;
-let doubleSize = size *2;
-document.write(doubleSize);
-let minSize = (doubleSize *2) - (size /2);
-document.write(minSize);
+alert("Hello World");
+let favoriteFood = "Chicken pasta";
+document.write(favoriteFood);
