e3b4c32e8454d860d2a44472566b55f58abe442b
uosukaine <uosukaine@gmail.com>
2022-03-21 09:55:28 +0100

    Update scripts.js
    Added variable pokemonList and objects in it according to the task instructions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-alert("Hello World");
-let favoriteFood = "Chicken pasta";
-document.write(favoriteFood);
+var pokemonList = [{
+	name: "Bulbasaur",
+	height: 2,
+	types: ["grass", "poison"]
+	},
+	{
+	name: "Charmander",
+	height: 2,
+	types: "fire"
+	}
+	{
+	name: "Squirtle",
+	height: 1,
+	types: "water"
+	}
+	{
+	name: "Caterpie",
+	height: 1,
+	types: "bug"
+	}
+	{
+	name: "Weedle",
+	height: 1,
+	types: ["bug", "poison"]
+	}];
+console.log(pokemonList);
