b8a5560c815652b76fcea4b056b92497dd1a2e5e
uosukaine <uosukaine@gmail.com>
2022-03-25 16:14:43 +0100

    Update scripts.js
    Changed pokemonList to IIFE format, added needed functions and updated forEach loop accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,53 @@
-// Listing pokemons and their features
-let pokemonList = [
-	{
-		name: "Bulbasaur",
-		height: 2,
-		types: ["grass", "poison"]
-	},
-	{
-		name: "Charmander",
-		height: 2,
-		types: ["fire"]
-	},
-	{
-		name: "Squirtle",
-		height: 1,
-		types: ["water"]
-	},
-	{
-		name: "Caterpie",
-		height: 1,
-		types: ["bug"]
-	},
-	{
-		name: "Weedle",
-		height: 1,
-		types: ["bug", "poison"]
-	}
-];
+// Listing pokemons and their features in IIFE format
+let pokemonRepository = (function () {
+	let pokemonList = [
+		{
+			name: "Bulbasaur",
+			height: 2,
+			types: ["grass", "poison"]
+		},
+		{
+			name: "Charmander",
+			height: 2,
+			types: ["fire"]
+		},
+		{
+			name: "Squirtle",
+			height: 1,
+			types: ["water"]
+		},
+		{
+			name: "Caterpie",
+			height: 1,
+			types: ["bug"]
+		},
+		{
+			name: "Weedle",
+			height: 1,
+			types: ["bug", "poison"]
+		}
+	]
 
+	// Defining getAll function to return pokemonList
+	function getAll () {
+		return pokemonList;
+	}
+
+	// Defining add function to add to pokemonList
+	function add(pokemon) {
+		pokemonList.push(pokemon);
+	}
+
+	// Returning getAll and add functions
+	return {
+		getAll: getAll,
+		add: add
+	}
+
+})();
 
 // Loop to list all the pokemons on the page
-pokemonList.forEach (function(pokemon) {
+pokemonRepository.getAll().forEach (function(pokemon) {
 	document.write("Name: " + pokemon.name + " (Height: " + pokemon.height + ". Types: " + pokemon.types + ".)");
 	
 	// Conditional text if Pokemon size is over 1.5
